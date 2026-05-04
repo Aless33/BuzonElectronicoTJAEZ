19de52ff6c06b82acce79e49b192ca6c9ee646c7
Spxcyyyy <alejandrosykess99@gmail.com>
2026-05-04 14:44:28 -0600

    App web

---
+++ buzon_electronico_tjaez/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for buzon_electronico_tjaez project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buzon_electronico_tjaez.settings')
+
+application = get_wsgi_application()
