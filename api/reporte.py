a5bfdcb8066b129107245b7a135009efaa9c6bc3
Spxcyyyy <alejandrosykess99@gmail.com>
2026-07-13 15:41:18 -0600

    Prueba del endpoint

--- api/reporte.py
+++ api/reporte.py
@@ -1,13 +1,17 @@
 import requests
-
-# 1. Login para obtener el token
+#Prueba
+'''
+Mismos pasos para realizar peticiones a los demas endpoints
+/ o  agregar tarea a redis para que cada noche realize el envio a un correo.
+'''
+# Login para obtener el token
 resp = requests.post(
     "http://localhost:8000/api/token/",
     json={"username": "kali", "password": "kali"},
 )
 access_token = resp.json()["access"]
 
-# 2. Pedir el reporte
+#  Pedir el reporte
 resp = requests.get(
     "http://localhost:8000/api/reportes/depositos-diarios/",
     params={"fecha": "2026-07-13"},
